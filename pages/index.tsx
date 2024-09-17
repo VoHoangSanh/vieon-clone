c6bdf50fb28196fe693f31c1b369c7af8b5d6966
Nhancoder284 <lebaonhan284@gmail.com>
2024-09-17 13:41:59 +0700

    icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-2xl text-green-400 ">lebaonhan</h1>
+    <h1 className="text-2xl text-green-400 ">Vieon</h1>
+    
     
   );
 }
