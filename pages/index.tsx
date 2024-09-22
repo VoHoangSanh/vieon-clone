a609de7ac8139be240b093b7111621ad17571169
vohoangsanh <vohoangsanhgr1990@gmail.com>
2024-09-22 19:36:50 +0700

    sanh'commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 
-    <h1 className="text-2xl text-green-400 ">Database</h1>
+    <h1 className="text-2xl text-green-400 ">Database new </h1>
     
     
   );
